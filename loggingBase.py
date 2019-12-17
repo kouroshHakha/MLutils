8e75b4ab510e1320c7521f9823c9895e23b9ae40
Kourosh Hakhamaneshi <kourosh_hakhamaneshi@berkeley.edu>
2019-12-17 07:12:39 -0800

    removed the hack on loggingBae

--- loggingBase.py
+++ loggingBase.py
@@ -4,7 +4,3 @@ class LoggingBase:
 
     def __init__(self, *args, **kwargs) -> None:
         pass
-
-    def main(self) -> None:
-        """Abstract class for main body of the class"""
-        raise NotImplementedError
