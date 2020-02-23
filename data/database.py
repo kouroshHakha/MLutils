4c89971a4fddb8838c38c1fcf1e0313b72a4269e
Kourosh Hakhamaneshi <kourosh_hakhamaneshi@berkeley.edu>
2020-02-22 22:08:45 -0800

    1. fixed TypeVar 2. now allow_repeat is not an optional parameter. It always assumes allow_repeat is True. 3. changed printing behaviour 4. added tot_freq as property

--- data/database.py
+++ data/database.py
@@ -1,31 +1,29 @@
-from typing import Type, Sequence, Any, Dict
+from typing import Sequence, Any, Dict, TypeVar, Type
 
 from collections import OrderedDict
 from datetime import datetime
 
 from utils.immutable import combine_hash, to_immutable
 
-T = Type
+T = TypeVar('T')
 
 class Database:
     """
     This is a database of hashable objects. It will keep track of the frequency and uniqueness of
-    objects added along with their last modified time stamp.
+    objects added along with their last modification time stamp.
     check for existence of a design O(1)
     asking for a certain top property O(n)
     asking for a sorted list return O(nlogn)
     insertion new elements O(1)
     """
 
-    def __init__(self, ttype: T, allow_repeat: bool = False):
+    def __init__(self, ttype: Type):
 
         # TODO: The following won't work as it is rn.
         # if not isinstance(ttype, Hashable):
         #     raise ValueError('Database only supports hashable objects')
 
         self._T = ttype
-        self._repeat = allow_repeat
-
         self._freq_dict: OrderedDict[T, int] = OrderedDict()
         self._list: Dict[datetime, T] = {}
         self._rlist: Dict[T, datetime] = {}
@@ -37,16 +35,9 @@ class Database:
                              f'type {dsn.__class__.__name__}')
 
         if dsn in self._freq_dict:
-            if self._repeat:
-                self._freq_dict[dsn] += 1
-                self._rlist[dsn] = dt
-                self._list[dt] = dsn
-            else:
-                # update time stamp
-                old_dt = self._rlist[dsn]
-                del self._list[old_dt]
-                self._list[dt] = dsn
-                self._rlist[dsn] = dt
+            self._freq_dict[dsn] += 1
+            self._rlist[dsn] = dt
+            self._list[dt] = dsn
         else:
             self._rlist[dsn] = dt
             self._list[dt] = dsn
@@ -72,14 +63,14 @@ class Database:
             return rep
 
         for i, dsn in enumerate(self._freq_dict):
-            rep += f'    {dsn},\n'
+            rep += f'    {repr(dsn)},\n'
             if i > 2:
                 break
 
         if nu > 4:
             rep += '    ...\n'
             last_dsn = next(reversed(self._freq_dict.keys()))
-            rep += f'    {last_dsn}'
+            rep += f'    {repr(last_dsn)}'
 
         return rep
 
@@ -104,7 +95,9 @@ class Database:
 
         return self._rlist[item]
 
-
+    @property
+    def tot_freq(self):
+        return sum(self._freq_dict.values())
 
 
 
