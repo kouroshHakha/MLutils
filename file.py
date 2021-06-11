5865903438761d24bad142ded06723884a032fb8
kourosh.hakhamaneshi <khakhama@aipg-ra-skx-03.ra.intel.com>
2021-06-11 09:20:10 -0700

    added hdf5 read/write to file.py

--- file.py
+++ file.py
@@ -1,10 +1,15 @@
-from typing import Any, Union
+from typing import Any, Union, Dict, Mapping
 
 from pathlib import Path
 
 from ruamel.yaml import YAML
 import pickle
+import h5py
+import numpy as np
+from numbers import Number
 
+
+PathLike = Union[str, Path]
 yaml = YAML(typ='safe')
 
 def read_yaml(fname: Union[str, Path]) -> Any:
@@ -121,3 +126,38 @@ def write_pickle(fname: Union[str, Path], obj: object, mkdir: bool = True) -> No
 
     with open(fpath, 'wb') as f:
         pickle.dump(obj, f)
+
+
+def read_hdf5(fpath: PathLike) -> Dict[str, Any]:
+    def _load_hdf5_helper(root: h5py.Group) -> Dict[str, Any]:
+        init_dict = {}
+        for k, v in root.items():
+            if isinstance(v, h5py.Dataset):
+                init_dict[k] = np.array(v)
+            elif isinstance(v, h5py.Group):
+                init_dict[k] = _load_hdf5_helper(v)
+            else:
+                raise ValueError(f'Does not support type {type(v)}')
+        return init_dict
+
+    with h5py.File(fpath, 'r') as f:
+        return _load_hdf5_helper(f)
+
+
+
+def write_hdf5(data_dict: Mapping[str, Any], fpath: PathLike) -> None:
+
+    def _save_as_hdf5_helper(obj: Mapping[str, Union[Mapping, np.ndarray]], root: h5py.File):
+        for k, v in obj.items():
+            if isinstance(v, np.ndarray):
+                root.create_dataset(name=k, data=v)
+            elif isinstance(v, dict):
+                grp = root.create_group(name=k)
+                _save_as_hdf5_helper(v, grp)
+            elif isinstance(v, Number):
+                root.create_dataset(name=k, data=v)
+            else:
+                raise ValueError(f'Does not support type {type(v)}')
+
+    with h5py.File(fpath, 'w') as root:
+        _save_as_hdf5_helper(data_dict, root)
